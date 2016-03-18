74cd2e1d102c139018b2f51c4112f454344ab9c5
chris saunders <chris@boxel.co.uk>
2016-03-18 16:11:13 +0000

    added new entities

--- app/router.js
+++ app/router.js
@@ -13,12 +13,19 @@ define(function (require) {
         },
 
         routes: {
-            'edi/:num/:id' : 'default'
+            'edi/:num/:id' : 'default',
+            'get/:type/:po' : 'get'
         },
 
         default: function (num, id) {
             require(['../app/views/index'], function (Content) {
-                self.app.setXML(new Content({ app: self.app, type: num, po: id }));
+                self.app.setXML(new Content({ app: self.app, type: num, num: id }));
+            });
+        },
+
+        get: function (type, po) {
+            require(['../app/views/get'], function (Content) {
+                self.app.setXML(new Content({ app: self.app, type: type, po: po }));
             });
         }
 
